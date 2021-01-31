d3205d77cc065aebbc6cb33e956918d32ab92dc7
Tyrion <lslabyrinth@163.com>
2021-02-01 00:14:35 +0800

    2021-02-01 gogogo

---
+++ src/pages/Admin/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+/**
+ * 管理页的路由组件
+ */
+
+export default class Admin extends Component {
+  render() {
+    return <div>aaa</div>;
+  }
+}
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./index.less";
+
+/**
+ * 登录页的路由组件
+ */
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <header className="login-header"></header>
+        <section className="login-container"></section>
+      </div>
+    );
+  }
+}
