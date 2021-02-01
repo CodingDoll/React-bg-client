b6c95f7b25beabcb83591b4f7a5fa448c3abe847
Tyrion <lslabyrinth@163.com>
2021-02-02 00:05:24 +0800

    login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,72 @@
 import React, { Component } from "react";
 import "./index.less";
+import logo from "./images/logo.png";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 /**
  * 登录页的路由组件
  */
 
 export default class Login extends Component {
+  onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   render() {
     return (
       <div className="login">
-        <header className="login-header"></header>
-        <section className="login-container"></section>
+        <header className="login-header">
+          <img src={logo} alt="logo" />
+          <h1>Doll</h1>
+        </header>
+        <section className="login-container">
+          <h2>Login</h2>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
       </div>
     );
   }
